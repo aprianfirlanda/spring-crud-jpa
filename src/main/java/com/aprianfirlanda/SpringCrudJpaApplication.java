485ba7593740bc8615881e772cd9133f5249de1b
aprianfirlanda <aprianfirlanda@gmail.com>
2023-04-12 22:02:54 +0700

    initial commit from start.spring.io

---
+++ src/main/java/com/aprianfirlanda/SpringCrudJpaApplication.java
@@ -0,0 +1,13 @@
+package com.aprianfirlanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudJpaApplication.class, args);
+	}
+
+}
